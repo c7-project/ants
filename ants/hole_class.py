37161da5cd5ee72908af9189fee48860acd19936
James Vickery <jamesevickery@users.noreply.github.com>
2016-11-08 14:51:05 +0000

    Split classes into modules

---
+++ ants/hole_class.py
@@ -0,0 +1,22 @@
+import pygame
+from random import randint
+
+
+class Hole(pygame.sprite.Sprite):
+    # Creates holes randomly spread across the arena, making sure they don't appear twice at the same point.
+    previous_coordinates = {}
+    keys = previous_coordinates.keys()
+    values = previous_coordinates.values()
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/hole01a.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        x = randint(1, 838)
+        y = randint(1, 538)
+        if x not in Hole.keys and y not in Hole.values:
+            self.rect.x = x
+            self.rect.y = y
+            Hole.previous_coordinates[self.rect.x] = self.rect.y
+        else:
+            self.__init__()
---
+++ ants/main.py
@@ -0,0 +1,94 @@
+from random import randint
+import pygame
+import ant_class
+import hole_class
+
+
+def display_text(screen, text, size, location):
+    # Create font
+    my_font = pygame.font.SysFont("monospace", size)
+    # Label text
+    label = my_font.render(text, 1, (250, 250, 250))
+    # Display text
+    screen.blit(label, location)
+
+
+def get_random_ish_direction(max_degrees=10):
+    return randint(-max_degrees, max_degrees)
+
+
+def main():
+    """
+    Main game code
+
+    Contains pygame setup and the main game loop
+    """
+    pygame.init()  # Initialise pygame
+    # Set window title
+    pygame.display.set_caption("Ants: An Artificial Intelligence Experiment by C7")
+    screen = pygame.display.set_mode((900, 600))  # Set window size
+    done = False  # The game loop is broken when done becomes True
+
+    # Load background image resource
+    bg = pygame.image.load("images/bg01a.jpg")
+
+    ant_list = [ant_class.Ant() for i in range(40)]
+    hole_list = [hole_class.Hole() for i in range(3)]
+
+    # Create groups for objects
+    ants = pygame.sprite.Group()
+    holes = pygame.sprite.Group()
+    rocks = pygame.sprite.Group()
+    food = pygame.sprite.Group()
+
+    # Add ants_list objects to ants list
+    for individual_ant in ant_list:
+        ants.add(individual_ant)
+
+    for individual_hole in hole_list:
+        holes.add(individual_hole)
+
+    # List of all sprites
+    all_sprites = pygame.sprite.Group()
+    # Add sprite groups to main group
+    all_sprites.add(ants, holes, rocks, food)
+    # pygame clock
+    clock = pygame.time.Clock()
+
+    while not done:  # Main game loop
+        clock.tick(60)  # Frame-rate
+        # Add background image, overlaying everything
+        screen.blit(bg, (0, 0))
+
+        # Rotate the ant - currently an experiment
+        # Will be used for when the ant moves around on its own
+        for ant in ant_list:
+            if ant.stop_count > 0:  # If need to wait for stop
+                ant.stop_count -= 1
+            else:  # Rotate and/or move or neither
+                if randint(0, 2) == 0:  # Rotate
+                    ant.rotate(get_random_ish_direction(14))
+                if randint(0, 7) > 0:  # Move
+                    ant.move(2)
+                if randint(0, 150) == 0:
+                    ant.stop(randint(10, 40))
+
+        # Draw all sprites group to the screen
+        all_sprites.draw(screen)
+        # Update all sprites
+        all_sprites.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # If close button
+                done = True  # Exit
+            if event.type == pygame.KEYDOWN:  # If key pressed
+                if event.key == pygame.K_ESCAPE:  # If 'esc' pressed
+                    done = True  # Exit
+
+        display_text(screen, "fps:" + str(int(round(clock.get_fps(), 0))), 14, (850, 0))
+
+        # Update display to show changes made in current iteration
+        pygame.display.update()
+
+
+main()
