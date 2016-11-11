e59f5599cb0c02e996824fdd709e2f6ee4ff8afa
Antonio Roldán Perez de la Rosa <Antonio7.9@hotmail.com>
2016-11-11 20:39:21 +0000

    Update hole_class.py
    For some reason, when trying to get the ants to appear at the coordinates where the holes are with the previous method,ants seem to only come from the [900,600] coordinate that is set by default within the hole_locations list. 
    I think the problem might be related to the way in which values are located, when I tried to implement this method with a way of making the holes appear at the given coordinates other than the use of the choice function, the main issue was that only the first hole in the list appeared, whereas the rest of the ants randomly started at the coordinates generated by the list. 
    However, when I set the range function within the main method that calls holes to say 20 and the number within holes_manager function to 5, just by probability the amount of holes that is needed appears, and ants randomly start from the coordinates generated by the list which also are the coordinates where the holes are. Everything seems to be working except for the holes clashing bug that was solved with the previous method. 
    It looks as if one option inevitably denied the other, either we try to fix the holes clashing bug using another method while maintaining the code I designed for the ants to appear at the desired points, or we try to get the ants to come from the holes using the previous method which seems unlikely to be doable from my perspective, since when made the same modification I made in the ants_class without altering the holes_class in any way, ants only appeared at that point that is stored in the holes_location list by default. 
    My personal view is, that despite losing the flexibility we would have by enabling the program to generate as many non-clashing holes as it can, only a maximum number of 10 holes I think would be created when running the program if we want it to look interesting, also the possibility of the holes clashing with such number of holes is considerably reduced with this code and after all it works. 
    If it is possible to implement a better algorithm to have both features in the code that would obviously be all the better, but I am afraid that might not be the case due to the nature of the problem itself.

--- ants/classes/hole_class.py
+++ ants/classes/hole_class.py
@@ -1,8 +1,24 @@
 import pygame
-from random import randint
+from random import randint, choice
 
 # List of the [x, y] coordinates of existing holes
-hole_locations = [[900, 600]]
+hole_locations = []
+
+def holes_manager(n):
+    """
+    Distributes the holes across evenly-sized
+    x_values = 875//n
+    y_values = 575//n
+    cx = 0
+    cy = 0
+    while True:
+        if cx >= 875 and cy <= 575:
+            break
+        cx += x_values
+        cy += y_values
+        x = rantint(1,cx)
+        y = randint(1,cy)
+        hole_locations.append([x, y])
 
 
 class Hole(pygame.sprite.Sprite):
@@ -15,34 +31,9 @@ class Hole(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         if len(hole_locations) > 60:
             raise ValueError("Exceeded maximum number of holes: 60")
-        self.rect.x, self.rect.y = get_valid_hole_location()
+        coordinates = choice(hole_locations)
+        self.rect.x = coordinates[0]
+        self.rect.y = coordinates[1]
+        
+        
 
-
-def get_valid_hole_location():
-    """
-    Run through hole_locations in search for new a new valid hole location
-    :return: The valid x and y coordinates
-    """
-    valid_location = False  # First assume the location is not valid
-    global hole_locations
-    x = 0
-    y = 0
-
-    while not valid_location:  # Loop while coordinates not valid
-        # Count to check the new hole is valid for all existing holes
-        valid_count = 0
-        x = randint(1, 838)  # Generate random x
-        y = randint(1, 538)  # And random y
-        for hole_loc in hole_locations:  # For each hole
-            # If new hole is to the left of, or above the existing hole
-            if x < hole_loc[0] - 61 or y < hole_loc[1] - 61:
-                valid_count += 1
-            # Else if new hole doesn't overlap existing hole
-            elif not (x < hole_loc[0] + 61 and y < hole_loc[1] + 61):
-                valid_count += 1
-        # If coordinates are valid for every existing hole
-        if valid_count == len(hole_locations):
-            valid_location = True
-
-    hole_locations.append([x, y])  # Add new coordinates to list
-    return x, y  # Return coordinates x and y
